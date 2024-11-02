9445e768b491fe415340721f5acb1eb8767949bd
z-hraa <107371337+z-hraa@users.noreply.github.com>
2024-11-02 18:56:30 +0000

    first commit, added operations

---
+++ main.py
@@ -0,0 +1,27 @@
+num1 = int(input(print("enter first number")))
+num2 = int(input(print("enter second number")))
+operation = input(print("enter operation"))
+
+# if operation is + we want to add num1 and num2
+
+if (operation == "+"):
+    num3 = num1 + num2
+    print(num3)
+
+if (operation == "/"):
+    num3 = num1 / num2
+    print(num3)
+
+if (operation == "-"):
+    num3 = num1 - num2 
+    print(num3) 
+     
+if (operation == "*"):
+    num3 = num1 * num2 
+    print(num3) 
+
+
+
+# task 1 - find out how to do powers i.e num1 ^ num2
+# task 2 - for division, ask if user wants remainder or decimal, if they want remainder then print i.e 15/2 = 7 remainder 1
+# task 3 - learn to do for loop, and do a loop. first ask the user how many numbers they want to enter. then etc.. 
